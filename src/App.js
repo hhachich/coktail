e3ea28b645c1a55f18dbdafc5fae840732006210
hhachich <hachichou.hassan@gmail.com>
2024-03-14 10:44:43 +0100

    Routage de la partie public

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
 import Service from './pages/Service';
 import Contact from './pages/Contact';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import Error from './utils/Error';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Home/>
-        <Service/>
-        <Contact/>
-      
-      </header>
+    <div className="App">   
+
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>}/> 
+        <Route path="/home" element={<Home/>}/> 
+        <Route path="/service" element={<Service/>}/> 
+        <Route path="/contact" element={<Contact/>}/> 
+        <Route path="*" element={<Error/>}/> 
+      </Routes>
+    </BrowserRouter>
+        
       
     </div>
   );
