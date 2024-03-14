5bd0da8dd7b2beb38c2f213554457266c8f29464
hhachich <hachichou.hassan@gmail.com>
2024-03-14 11:09:27 +0100

    Navigation Router Link et Header

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import Header from '../components/public/Header';
 const Service = () => {
     return (
         <div>
+             <Header/>
            Service works !
         </div>
     );
