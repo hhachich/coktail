b1869694fb7369bc234c879b5ad813340f05f67f
hhachich <hachichou.hassan@gmail.com>
2024-03-15 09:40:53 +0100

    Branchement du login - Form React et State

--- src/pages/auth/Longing.js
+++ src/pages/auth/Longing.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './auth.css';
 const Longing = () => {
+
+    const[login,setLogin]=useState('')
+    const[password,setPassword]=useState('')
+
+    const onSubmit=(e)=>{
+        e.preventDefault()
+        console.log('formulaire')
+    }
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <div className='group'>
                 <label htmlFor='login'>Identifiant</label>
-                <input type='text' name='login'></input>
+                <input type='text' name='login' value={login} onChange={e=>setLogin(e.target.value)}></input>
             </div>
             <div className='group'>
                 <label htmlFor='password'>Mot de passe</label>
-                <input type='text' name='password'></input>
+                <input type='text' name='password' value={password} onChange={e=>setPassword(e.target.value)}></input>
             </div>
             <div className='group'>
                 <button>Connexion</button>
