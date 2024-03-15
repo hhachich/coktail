7fa73ea59bb6d634e804c853124f575a70b985fa
hhachich <hachichou.hassan@gmail.com>
2024-03-15 11:44:31 +0100

    Le hook useEffect

--- src/pages/admin/user/User.js
+++ src/pages/admin/user/User.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const User = () => {
     
     let navigate=useNavigate();
+    
+    useEffect(() => {
+        console.log('useEffect')
+        
+    }, [])
 
     const marcel=(userId)=>{
         console.log("click "+userId );
